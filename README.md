da3c409a34b166c5d43dbd7bc7ead5ef16eb3846
Maxime Verdy <maximeverdy@gmail.com>
2022-03-07 20:52:44 +0100

    screenshot ok

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 #### présentant une pensée poétique
 Projet permettant de travailler sur les animations GSAP
 
-![sreenshot](screenshot1.gif)
+![sreenshot](screenshot.gif)
--- animations.js
+++ animations.js
@@ -5,4 +5,4 @@ tl.from("img", { x: "-30%", opacity: 0 })
      .from(".container", { x: "-40%", backdropFilter: "blur(0px)", opacity: 0, delay: 0, duration: 3.5 }, "-=.8")
      .from(".txt1", { y: -30, opacity: 0, stagger: 0.2, duration: 1 }, "-=.5")
      .from(".txt2", { y: -30, opacity: 0, delay: 0.5, stagger: 0.2, duration: 1 }, "-=.5")
-     .from("h1", { y: 20, clipPath: "inset(0 0 100% 0)" }, "-=.8");
+     .from("h1", { y: 20, delay: 1.5, clipPath: "inset(0 0 100% 0)" }, "-=.8");
